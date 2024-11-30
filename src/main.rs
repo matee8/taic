2e336d7d639819c6992e57e59b9f5779579496a5
matee8 <molnarmatenorbert@gmail.com>
2024-11-30 13:30:21 +0100

    Implemented argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "llmcli", version, about)]
+struct Cli {
+    #[arg(short, long, value_name = "LLM")]
+    model: String,
+    #[arg(long, value_name = "KEY")]
+    api_key: String,
+    #[arg(long, value_name = "ENDPOINT")]
+    api_endpoint: Option<String>,
+    #[arg(short, long, value_name = "INTEGER", default_value_t = 0.7)]
+    temperature: f32,
+    #[arg(short, long, value_name = "STRING")]
+    prompt: String,
+    #[arg(short, long)]
+    reset: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("{}", cli.model);
 }
