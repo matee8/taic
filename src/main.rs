63022cb396c28e4ce338c3424884259a2dc71cfe
matee8 <molnarmatenorbert@gmail.com>
2024-12-22 17:04:21 +0100

    Project reset

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,3 @@
-use clap::Parser;
-
-#[derive(Parser)]
-#[command(name = "llmcli", version, about)]
-struct Cli {
-    #[arg(short, long, value_name = "LLM")]
-    model: String,
-    #[arg(long, value_name = "KEY")]
-    api_key: String,
-    #[arg(long, value_name = "ENDPOINT")]
-    api_endpoint: Option<String>,
-    #[arg(short, long, value_name = "INTEGER", default_value_t = 0.7)]
-    temperature: f32,
-    #[arg(short, long, value_name = "STRING")]
-    prompt: String,
-    #[arg(short, long)]
-    reset: bool,
-}
-
 fn main() {
-    let cli = Cli::parse();
-
-    println!("{}", cli.model);
+    println!("Hello, world!");
 }
