3834198e25469a3691f46c09e5ae542c5d1a2650
matee8 <molnarmatenorbert@gmail.com>
2024-12-24 21:53:14 +0100

    feat: implement main interaction loop

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,71 @@
-use clap::Parser as _;
-use llmcli::cli::{Args, Command};
+use std::{
+    io::{self, Write as _},
+    process,
+};
 
-fn main() {
+use clap::Parser as _;
+use llmcli::{
+    chatbots::dummy::DummyChatbot,
+    cli::{Args, Command},
+    Chatbot, ChatbotError, Message, Role,
+};
+use thiserror::Error;
+
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
-    match args.command {
-        Command::Gemini { model } => {
-            println!("Starting Gemini chatbot with model {model:?}...");
-        }
-        Command::Dummy => {
-            println!("Starting Dummy chatbot...");
-        }
+    if let Err(err) = run_chat(match args.command {
+        Command::Dummy => DummyChatbot::new(),
         _ => {
-            println!("Unknown command! Stopping...");
+            eprintln!("Error: Unknown chatbot.");
+            process::exit(1);
         }
+    })
+    .await
+    {
+        eprintln!("Error: {err}");
+        process::exit(1);
+    }
+}
+
+#[derive(Debug, Error)]
+enum ChatError {
+    #[error("{0}")]
+    Io(#[from] io::Error),
+    #[error("{0}")]
+    Chatbot(#[from] ChatbotError),
+}
+
+#[expect(
+    clippy::single_call_fn,
+    reason = r#"Traits with `async fn` have limitations using dynamic dispatch. 
+                `async_trait` uses the heap which isn't the optimal solution.
+                This function instead uses static dispatch to work around those."#
+)]
+async fn run_chat<C>(chatbot: C) -> Result<(), ChatError>
+where
+    C: Chatbot + Send + Sync,
+{
+    let mut hist = Vec::new();
+
+    loop {
+        print!("You: ");
+        io::stdout().flush()?;
+
+        let mut prompt = String::new();
+        io::stdin().read_line(&mut prompt)?;
+
+        let _: Option<char> = prompt.pop();
+
+        if prompt.trim().is_empty() {
+            continue;
+        }
+
+        hist.push(Message::new(Role::User, prompt));
+
+        let resp = chatbot.send_message(&hist).await?;
+        println!("{resp}");
+        hist.push(Message::new(Role::Assistant, resp));
     }
 }
