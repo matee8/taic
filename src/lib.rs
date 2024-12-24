57c60b6c797bcb807a89d1b0b26e15fa6c916629
matee8 <molnarmatenorbert@gmail.com>
2024-12-24 19:39:03 +0100

    feat: Define Chatbot trait and Message struct

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use core::future::Future;
+
+use thiserror::Error;
+
+#[non_exhaustive]
+pub enum Role {
+    System,
+    User,
+    Assistant,
+}
+
+pub struct Message<'content> {
+    role: Role,
+    content: &'content str,
+}
+
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum ChatbotError {
+    #[error("Invalid API key.")]
+    InvalidApiKey,
+    #[error("Authentication error.")]
+    AuthenticationError,
+    #[error("Rate limit exceeded.")]
+    RateLimitExceeded,
+    #[error("Server error.")]
+    ServerError,
+    #[error("Model overloaded.")]
+    ModelOverloaded,
+    #[error("Network error: {0}.")]
+    NetworkError(String),
+    #[error("Unexpected response: {0}.")]
+    UnexpectedResponse(String),
+}
+
+pub trait Chatbot {
+    fn name() -> &'static str;
+
+    fn send_message(
+        &self,
+        messages: &[Message<'_>],
+    ) -> impl Future<Output = Result<String, ChatbotError>> + Send + Sync;
+}
